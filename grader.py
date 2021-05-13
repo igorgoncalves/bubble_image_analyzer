e4cb8c6c3d6e8e01f848a7043a3574b81a3ccbbc
Igor Gonçalves <igor_goncalves@live.com>
2021-05-13 12:37:03 -0300

    define funcões reaproveitaveis e melhorias na legibilidade do código

--- grader.py
+++ grader.py
@@ -1,9 +1,10 @@
+import copy
 import statistics
 import cv2
 import numpy as np
 import imutils
-import four_point
 from imutils import contours
+import four_point
 
 
 def extract_size(item):
@@ -24,8 +25,9 @@ def is_between(value, limit, error_margin=2):
 
 
 def show_contours(cnts, background):
-    cv2.drawContours(background, cnts, -1, 255, -1)
-    cv2.imshow("show", background)
+    image = copy.deepcopy(background)
+    cv2.drawContours(image, cnts, -1, 255, -1)
+    cv2.imshow("show", image)
     cv2.waitKey(0)
 
 
@@ -51,52 +53,69 @@ def apply_threshold(cuted_image):
     return thresh
 
 
-def get_contours(thresh):
-    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def get_contours(image):
+    cnts = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     return cnts
 
 
-def main(IMAGE_PATH="omr2.png",
-         NUM_LINES=5,
-         NUM_OPTIONS=5,
-         APPROXION_ACCURACY_TAX=0.02):
-
-    image = cv2.imread(IMAGE_PATH)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    edged = cv2.Canny(blurred, 75, 200)
-
-    all_contours = cv2.findContours(
-        edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    all_contours = imutils.grab_contours(all_contours)
-    doc_cnt = None
-
+def get_doc_contour(all_contours, accuracy_tax):
     if len(all_contours) > 0:
         all_sorted_contours = sorted(
             all_contours,
             key=cv2.contourArea,
             reverse=True
         )
-        for contour in all_sorted_contours:
-            perimeter_contour = cv2.arcLength(contour, True)
-            epsilon = APPROXION_ACCURACY_TAX * perimeter_contour
-            approximated_contour = cv2.approxPolyDP(
-                contour, epsilon, True)
-            if len(approximated_contour) == 4:
-                doc_cnt = approximated_contour
-                break
+    for contour in all_sorted_contours:
+        perimeter_contour = cv2.arcLength(contour, True)
+        epsilon = accuracy_tax * perimeter_contour
+        approximated_contour = cv2.approxPolyDP(
+            contour, epsilon, True)
+        if len(approximated_contour) == 4:
+            doc_cnt = approximated_contour
+            return doc_cnt
 
-    # paper = four_point.four_point_transform(image, doc_cnt.reshape(4, 2))
+
+
+def main(image_path="omr2.png",
+         num_lines=5,
+         num_options=5,
+         accuracy_tax=0.02):
+
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blurred, 75, 200)
+
+    all_contours = cnts = get_contours(edged)
+
+    doc_cnt = get_doc_contour(all_contours, accuracy_tax)    
+
+    paper = four_point.four_point_transform(image, doc_cnt.reshape(4, 2))
+
     warped = four_point.four_point_transform(gray, doc_cnt.reshape(4, 2))
+
+    show_contours([], image)
+    show_contours([], paper)
+
     thresh = apply_threshold(warped)
+
+    show_contours([], thresh)
+
     cnts = get_contours(thresh)
+
+    show_contours(cnts, paper)
+
     (mean_width, mean_height) = calc_sizes_avg(cnts)
+
     valid_bubbles_options = filter_contours(cnts, mean_width, mean_height)
 
-    for (_, i) in enumerate(np.arange(0, len(valid_bubbles_options), NUM_LINES)):
+    show_contours(valid_bubbles_options, paper)
+    valid_bubbles_options.reverse()
+
+    for (_, i) in enumerate(np.arange(0, len(valid_bubbles_options), num_lines)):
         cnts = contours.sort_contours(
-            valid_bubbles_options[i: i + NUM_OPTIONS])[0]
+            valid_bubbles_options[i: i + num_options])[0]
         list_num_pixels = list(
             map(lambda item: sum_pixels(item, thresh), cnts))
         mean_num_pixels = statistics.mean(list_num_pixels)
@@ -106,3 +125,4 @@ def main(IMAGE_PATH="omr2.png",
 
 
 main()
+# main("form.png", num_lines=6, num_options=6)
